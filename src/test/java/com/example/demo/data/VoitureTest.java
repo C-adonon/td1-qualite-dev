87e3991f4542940ec67d693f8b2a75f93aaaa56b
Chloé Adonon <99622570+C-adonon@users.noreply.github.com>
2026-06-03 15:06:13 +0200

    test créer voiture avec id

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -10,11 +10,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class VoitureTest {
 
     @Test
-    void creerVoiture(){
+    void creerVoiture() {
         Voiture voiture = new Voiture("Porsche", 70000);
-        Assert.isTrue(voiture.getMarque().equals("Porsche"),"Doit être Porsche");
-        Assert.isTrue(voiture.getPrix()== 70000,"Prix doit être 70000");
-        Assert.isTrue(voiture.getId()== 0, "Doit être 0");
+        Assert.isTrue(voiture.getMarque().equals("Porsche"), "Doit être Porsche");
+        Assert.isTrue(voiture.getPrix() == 70000, "Prix doit être 70000");
+        Assert.isTrue(voiture.getId() == 0, "Doit être 0");
+    }
+
+    @Test
+    void creerVoitureAvecId() {
+        Voiture voiture = new Voiture("Fiat", 7000);
+        Assert.isTrue(voiture.getMarque().equals("Fiat"), "Doit être une Fiat");
+        Assert.isTrue(voiture.getPrix() == 7000, "Prix doit être 7000");
+        Assert.isTrue(voiture.getId() == 2, "Doit être 2");
     }
 
 }
