464f1ecd365f6b0bc82e7010583bcbfe53a53f24
Chloé Adonon <99622570+C-adonon@users.noreply.github.com>
2026-06-03 16:30:12 +0200

    init web test

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -14,4 +14,16 @@ public class StatistiqueTests {
     @MockBean
     StatistiqueImpl statistiqueImpl;
 
+    @Test
+    public void getStatistiques() throws Exception {
+        doNothing().when(statistiqueImpl).ajouter(new Voiture("Ferrari", 100000));
+        when(statistiqueImpl.prixMoyen()).thenReturn(new Echantillon(1, 100000));
+        mockMvc.perform(get("/statistique"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nombreDeVoitures").value(1))
+                .andExpect(jsonPath("$.prixMoyen").value(100000));
+                .andReturn();
+    }
+
 }
